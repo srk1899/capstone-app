d222cea389d31bfc5cf505cb2eef4f17765c15a2
Sunny Kashyap <sunny.kashyap@bellblazetech.com>
2025-07-15 16:33:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 
-app.get('/', (req, res) => res.send('Hello from Capstone App!'));
+app.get('/', (req, res) => res.send('Hello from Task-12!'));
 app.listen(port, '0.0.0.0', () => console.log(`Server is running on http://0.0.0.0:${port}`));
                                                                                                                                        
