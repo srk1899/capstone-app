5215b8a22d20741c4f5daced9b6a04dbb64f93f4
Sunny Kashyap <sunny.kashyap@bellblazetech.com>
2025-07-15 16:48:49 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-task-14-bucket"
+    bucket         = "task-12bucket"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
