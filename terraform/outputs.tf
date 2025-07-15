faf1da520b3b7431b79dc182b5bd82ee7239930d
Sunny Kashyap <sunny.kashyap@bellblazetech.com>
2025-07-15 15:11:17 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,21 @@
-output "alb_dns" {
-  value = aws_lb.alb.dns_name
+output "ecr_repository_url" {
+  value = aws_ecr_repository.my_app.repository_url
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
+output "green_tg_arn" {
+  value = aws_lb_target_group.green.arn
+}
+
+output "blue_tg_arn" {
+  value = aws_lb_target_group.blue.arn
+}
+
+output "alb_listener_arn" {
+  value = aws_lb_listener.http.arn
+}
+output "alb_dns_name" {
+  value = aws_lb.app_alb.dns_name
 }
